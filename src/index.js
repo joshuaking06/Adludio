3052aeac0969a95942e08da8bde782804f8a107b
joshuaking06 <joshua.king0692@gmail.com>
2019-12-02 17:29:06 +0000

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+import './style.scss'
+const Login = () => {
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	)
+}
+
+ReactDOM.render(<Login />, document.getElementById('root'))
