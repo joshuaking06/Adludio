226066249e544dc019a7d58a5aee48cf35563173
joshuaking06 <joshua.king0692@gmail.com>
2019-12-02 19:10:17 +0000

    register button done

--- src/index.js
+++ src/index.js
@@ -14,6 +14,26 @@ const Login = () => {
 			</div>
 			<div className="right">
 				<img src="./asset3.png" />
+				<h4>Register</h4>
+				<form>
+					<label>
+						Full Name
+						<input type="text" name="fullname" />
+					</label>
+
+					<label>
+						Email
+						<input type="email" name="email" />
+					</label>
+
+					<label>
+						Password
+						<input type="password" name="password" />
+					</label>
+				</form>
+				<div className="buttons">
+					<button id="register">Register</button>
+				</div>
 			</div>
 		</div>
 	)
